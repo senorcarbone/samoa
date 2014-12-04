951cac9b14d514e2130887d60304d618a9e644aa
senorcarbone <seniorcarbone@gmail.com>
2014-12-04 23:48:26 +0100

    Initial wrappers for Apache Flink added

---
+++ samoa-flink/src/main/java/com/yahoo/labs/flink/topology/impl/FlinkEntranceProcessingItem.java
@@ -0,0 +1,10 @@
+package com.yahoo.labs.flink.topology.impl;
+
+
+import com.yahoo.labs.samoa.topology.AbstractEntranceProcessingItem;
+
+import java.io.Serializable;
+
+public class FlinkEntranceProcessingItem extends AbstractEntranceProcessingItem
+		implements FlinkProcessingNode, Serializable {
+}
